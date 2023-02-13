9ba0518e83c2ec9e80c9c7003c32a075316a43f0
Nicolas CALENGE <nicolas.calenge@wisper.io>
2023-02-13 15:22:44 +0100

    chore: rebuild

--- src/Cron.tsx
+++ src/Cron.tsx
@@ -1,118 +1,379 @@
-import React from 'react'
+import React, { useState, useCallback, useEffect, useRef, useMemo } from 'react'
+import { Button, FormGroup } from '@mui/material'
+import { CronProps, PeriodType } from './types'
+import Period from './fields/Period'
+import MonthDays from './fields/MonthDays'
+import Months from './fields/Months'
+import Hours from './fields/Hours'
+import Minutes from './fields/Minutes'
+import WeekDays from './fields/WeekDays'
+import { classNames, setError, usePrevious } from './utils'
+import { DEFAULT_LOCALE_EN } from './locale'
+import { setValuesFromCronString, getCronStringFromValues } from './converter'
 
-export default function Cron() {
-  return <h1>ICI</h1>
-  // return (
-  //   <div className={internalClassName}>
-  //     <h1>ICI</h1>
-  //     <FormGroup>
-  //       <Period
-  //         value={periodForRender}
-  //         setValue={setPeriod}
-  //         locale={locale}
-  //         className={className}
-  //         disabled={disabled}
-  //         readOnly={readOnly}
-  //         shortcuts={shortcuts}
-  //         {...selectProps}
-  //       />
-  //     </FormGroup>
+import './styles.css'
 
-  //     {periodForRender === 'reboot' ? (
-  //       clearButtonNode
-  //     ) : (
-  //       <>
-  //         {periodForRender === 'year' && (
-  //           <FormGroup>
-  //             <Months
-  //               value={months}
-  //               setValue={setMonths}
-  //               locale={locale}
-  //               className={className}
-  //               humanizeLabels={humanizeLabels}
-  //               disabled={disabled}
-  //               readOnly={readOnly}
-  //               period={periodForRender}
-  //               {...selectProps}
-  //             />
-  //           </FormGroup>
-  //         )}
+export default function Cron(props: CronProps) {
+  const {
+    clearButton = true,
+    clearButtonProps = {},
+    clearButtonAction = 'fill-with-every',
+    locale = DEFAULT_LOCALE_EN,
+    value = '',
+    setValue,
+    displayError = true,
+    onError,
+    className,
+    defaultPeriod = 'day',
+    allowEmpty = 'for-default-value',
+    humanizeLabels = true,
+    humanizeValue = false,
+    disabled = false,
+    readOnly = false,
+    leadingZero = false,
+    shortcuts = [
+      '@yearly',
+      '@annually',
+      '@monthly',
+      '@weekly',
+      '@daily',
+      '@midnight',
+      '@hourly',
+    ],
+    clockFormat,
+    ...selectProps
+  } = props
+  const internalValueRef = useRef<string>(value)
+  const defaultPeriodRef = useRef<PeriodType>(defaultPeriod)
+  const [period, setPeriod] = useState<PeriodType | string | undefined>()
+  const [monthDays, setMonthDays] = useState<number[] | undefined>()
+  const [months, setMonths] = useState<number[] | undefined>()
+  const [weekDays, setWeekDays] = useState<number[] | undefined>()
+  const [hours, setHours] = useState<number[] | undefined>()
+  const [minutes, setMinutes] = useState<number[] | undefined>()
+  const [error, setInternalError] = useState<boolean>(false)
+  const [valueCleared, setValueCleared] = useState<boolean>(false)
+  const previousValueCleared = usePrevious(valueCleared)
+  const localeJSON = JSON.stringify(locale)
 
-  //         {(periodForRender === 'year' || periodForRender === 'month') && (
-  //           <FormGroup>
-  //             <MonthDays
-  //               value={monthDays}
-  //               setValue={setMonthDays}
-  //               locale={locale}
-  //               className={className}
-  //               weekDays={weekDays}
-  //               disabled={disabled}
-  //               readOnly={readOnly}
-  //               leadingZero={leadingZero}
-  //               period={periodForRender}
-  //               {...selectProps}
-  //             />
-  //           </FormGroup>
-  //         )}
+  useEffect(
+    () => {
+      setValuesFromCronString(
+        value,
+        setInternalError,
+        onError,
+        allowEmpty,
+        internalValueRef,
+        true,
+        locale,
+        shortcuts,
+        setMinutes,
+        setHours,
+        setMonthDays,
+        setMonths,
+        setWeekDays,
+        setPeriod
+      )
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    []
+  )
 
-  //         {(periodForRender === 'year' ||
-  //           periodForRender === 'month' ||
-  //           periodForRender === 'week') && (
-  //             <FormGroup>
-  //               <WeekDays
-  //                 value={weekDays}
-  //                 setValue={setWeekDays}
-  //                 locale={locale}
-  //                 className={className}
-  //                 humanizeLabels={humanizeLabels}
-  //                 monthDays={monthDays}
-  //                 disabled={disabled}
-  //                 readOnly={readOnly}
-  //                 period={periodForRender}
-  //                 {...selectProps}
-  //               />
-  //             </FormGroup>
-  //           )}
+  useEffect(
+    () => {
+      if (value !== internalValueRef.current) {
+        setValuesFromCronString(
+          value,
+          setInternalError,
+          onError,
+          allowEmpty,
+          internalValueRef,
+          false,
+          locale,
+          shortcuts,
+          setMinutes,
+          setHours,
+          setMonthDays,
+          setMonths,
+          setWeekDays,
+          setPeriod
+        )
+      }
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [value, internalValueRef, localeJSON, allowEmpty, shortcuts]
+  )
 
-  //         <div>
-  //           {periodForRender !== 'minute' && periodForRender !== 'hour' && (
-  //             <FormGroup>
-  //               <Hours
-  //                 value={hours}
-  //                 setValue={setHours}
-  //                 locale={locale}
-  //                 className={className}
-  //                 disabled={disabled}
-  //                 readOnly={readOnly}
-  //                 leadingZero={leadingZero}
-  //                 clockFormat={clockFormat}
-  //                 period={periodForRender}
-  //                 {...selectProps}
-  //               />
-  //             </FormGroup>
-  //           )}
+  useEffect(
+    () => {
+      // Only change the value if a user touched a field
+      // and if the user didn't use the clear button
+      if (
+        (period ||
+          minutes ||
+          months ||
+          monthDays ||
+          weekDays ||
+          hours ||
+          minutes) &&
+        !valueCleared &&
+        !previousValueCleared
+      ) {
+        const cron = getCronStringFromValues(
+          period || defaultPeriodRef.current,
+          months,
+          monthDays,
+          weekDays,
+          hours,
+          minutes,
+          humanizeValue
+        )
 
-  //           {periodForRender !== 'minute' && (
-  //             <FormGroup>
-  //               <Minutes
-  //                 value={minutes}
-  //                 setValue={setMinutes}
-  //                 locale={locale}
-  //                 period={periodForRender}
-  //                 className={className}
-  //                 disabled={disabled}
-  //                 readOnly={readOnly}
-  //                 leadingZero={leadingZero}
-  //                 clockFormat={clockFormat}
-  //                 {...selectProps}
-  //               />
-  //             </FormGroup>
-  //           )}
+        setValue(cron)
+        internalValueRef.current = cron
 
-  //           {clearButtonNode}
-  //         </div>
-  //       </>
-  //     )}
-  //   </div>
-  // )
+        onError && onError(undefined)
+        setInternalError(false)
+      } else if (valueCleared) {
+        setValueCleared(false)
+      }
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [
+      period,
+      monthDays,
+      months,
+      weekDays,
+      hours,
+      minutes,
+      humanizeValue,
+      valueCleared,
+    ]
+  )
+
+  const handleClear = useCallback(
+    () => {
+      setMonthDays(undefined)
+      setMonths(undefined)
+      setWeekDays(undefined)
+      setHours(undefined)
+      setMinutes(undefined)
+
+      // When clearButtonAction is 'empty'
+      let newValue = ''
+
+      const newPeriod =
+        period !== 'reboot' && period ? period : defaultPeriodRef.current
+
+      if (newPeriod !== period) {
+        setPeriod(newPeriod)
+      }
+
+      // When clearButtonAction is 'fill-with-every'
+      if (clearButtonAction === 'fill-with-every') {
+        const cron = getCronStringFromValues(
+          newPeriod,
+          undefined,
+          undefined,
+          undefined,
+          undefined,
+          undefined
+        )
+
+        newValue = cron
+      }
+
+      setValue(newValue)
+      internalValueRef.current = newValue
+
+      setValueCleared(true)
+
+      if (allowEmpty === 'never' && clearButtonAction === 'empty') {
+        setInternalError(true)
+        setError(onError, locale)
+      } else {
+        onError && onError(undefined)
+        setInternalError(false)
+      }
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [period, setValue, onError, clearButtonAction]
+  )
+
+  const internalClassName = useMemo(
+    () =>
+      classNames({
+        'react-js-cron': true,
+        'react-js-cron-error': error && displayError,
+        'react-js-cron-disabled': disabled,
+        'react-js-cron-read-only': readOnly,
+        [`${className}`]: !!className,
+        [`${className}-error`]: error && displayError && !!className,
+        [`${className}-disabled`]: disabled && !!className,
+        [`${className}-read-only`]: readOnly && !!className,
+      }),
+    [className, error, displayError, disabled, readOnly]
+  )
+
+  const {
+    className: clearButtonClassNameProp,
+    ...otherClearButtonProps
+  } = clearButtonProps
+  const clearButtonClassName = useMemo(
+    () =>
+      classNames({
+        'react-js-cron-clear-button': true,
+        [`${className}-clear-button`]: !!className,
+        [`${clearButtonClassNameProp}`]: !!clearButtonClassNameProp,
+      }),
+    [className, clearButtonClassNameProp]
+  )
+
+  const otherClearButtonPropsJSON = JSON.stringify(otherClearButtonProps)
+  const clearButtonNode = useMemo(
+    () => {
+      if (clearButton && !readOnly) {
+        return (
+          <Button
+            style={{ marginLeft: '10px' }}
+            className={clearButtonClassName}
+            variant='contained'
+            color='secondary'
+            disabled={disabled}
+            onClick={handleClear}
+          >
+            {locale.clearButtonText || DEFAULT_LOCALE_EN.clearButtonText}
+          </Button>
+        )
+      }
+
+      return null
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [
+      clearButton,
+      readOnly,
+      localeJSON,
+      clearButtonClassName,
+      disabled,
+      otherClearButtonPropsJSON,
+      handleClear,
+    ]
+  )
+
+  const periodForRender = period || defaultPeriodRef.current
+  console.log("TOTOTOO")
+  return (
+    <div className={internalClassName}>
+      <h1>ICI</h1>
+      <FormGroup>
+        <Period
+          value={periodForRender}
+          setValue={setPeriod}
+          locale={locale}
+          className={className}
+          disabled={disabled}
+          readOnly={readOnly}
+          shortcuts={shortcuts}
+          {...selectProps}
+        />
+      </FormGroup>
+
+      {periodForRender === 'reboot' ? (
+        clearButtonNode
+      ) : (
+        <>
+          {periodForRender === 'year' && (
+            <FormGroup>
+              <Months
+                value={months}
+                setValue={setMonths}
+                locale={locale}
+                className={className}
+                humanizeLabels={humanizeLabels}
+                disabled={disabled}
+                readOnly={readOnly}
+                period={periodForRender}
+                {...selectProps}
+              />
+            </FormGroup>
+          )}
+
+          {(periodForRender === 'year' || periodForRender === 'month') && (
+            <FormGroup>
+              <MonthDays
+                value={monthDays}
+                setValue={setMonthDays}
+                locale={locale}
+                className={className}
+                weekDays={weekDays}
+                disabled={disabled}
+                readOnly={readOnly}
+                leadingZero={leadingZero}
+                period={periodForRender}
+                {...selectProps}
+              />
+            </FormGroup>
+          )}
+
+          {(periodForRender === 'year' ||
+            periodForRender === 'month' ||
+            periodForRender === 'week') && (
+              <FormGroup>
+                <WeekDays
+                  value={weekDays}
+                  setValue={setWeekDays}
+                  locale={locale}
+                  className={className}
+                  humanizeLabels={humanizeLabels}
+                  monthDays={monthDays}
+                  disabled={disabled}
+                  readOnly={readOnly}
+                  period={periodForRender}
+                  {...selectProps}
+                />
+              </FormGroup>
+            )}
+
+          <div>
+            {periodForRender !== 'minute' && periodForRender !== 'hour' && (
+              <FormGroup>
+                <Hours
+                  value={hours}
+                  setValue={setHours}
+                  locale={locale}
+                  className={className}
+                  disabled={disabled}
+                  readOnly={readOnly}
+                  leadingZero={leadingZero}
+                  clockFormat={clockFormat}
+                  period={periodForRender}
+                  {...selectProps}
+                />
+              </FormGroup>
+            )}
+
+            {periodForRender !== 'minute' && (
+              <FormGroup>
+                <Minutes
+                  value={minutes}
+                  setValue={setMinutes}
+                  locale={locale}
+                  period={periodForRender}
+                  className={className}
+                  disabled={disabled}
+                  readOnly={readOnly}
+                  leadingZero={leadingZero}
+                  clockFormat={clockFormat}
+                  {...selectProps}
+                />
+              </FormGroup>
+            )}
+
+            {clearButtonNode}
+          </div>
+        </>
+      )}
+    </div>
+  )
 }
